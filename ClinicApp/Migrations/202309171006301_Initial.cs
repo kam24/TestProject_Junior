5c2b246de2ad28e7f6198eda12593fa83d259163
Kamil Zalyalov <kamil.3alyalov@gmail.com>
2023-09-17 13:47:56 +0300

    Revert "Revert "Change EF version to 6.4.4""
    This reverts commit 9e7f25752be2b44f97c2121e1e8769c6fac9da6e.

---
+++ ClinicApp/Migrations/202309171006301_Initial.cs
@@ -0,0 +1,18 @@
+﻿namespace ClinicApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.PatientCards", "Age", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.PatientCards", "Age");
+        }
+    }
+}
